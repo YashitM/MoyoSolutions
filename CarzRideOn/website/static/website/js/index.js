6a0ea28c6665baebaf7df4b7e0030df713a853a5
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2017-12-24 07:50:18 +0530

    Location Selection Complete

--- CarzRideOn/website/static/website/js/index.js
+++ CarzRideOn/website/static/website/js/index.js
@@ -1,65 +1,79 @@
 //Google Maps
 
-    defaultLatLong = {lat: 12.978718, lng: 77.589731};
+defaultLatLong = {lat: 12.978718, lng: 77.589731};
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: defaultLatLong,
-      zoom: 13,
-      mapTypeId: 'roadmap'
-    });
+var map = new google.maps.Map(document.getElementById('map'), {
+  center: defaultLatLong,
+  zoom: 13,
+  mapTypeId: 'roadmap'
+});
 
-    var input = document.getElementById('location_input');
-    var card = document.getElementById('pac-card');
+var input = document.getElementById('location_input');
+var card = document.getElementById('pac-card');
 
-    var autocomplete = new google.maps.places.Autocomplete(input);
+var autocomplete = new google.maps.places.Autocomplete(input);
 
-    autocomplete.bindTo('bounds',map);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+autocomplete.bindTo('bounds',map);
+// map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
 
-    var marker = new google.maps.Marker({
-        map: map,
-        position: defaultLatLong,
-        draggable: true,
-        clickable: true
-    });
+var marker = new google.maps.Marker({
+    map: map,
+    position: defaultLatLong,
+    draggable: true,
+    clickable: true
+});
 
-    google.maps.event.addListener(marker, 'dragend', function(marker){
-        var latLng = marker.latLng;
-        currentLatitude = latLng.lat();
-        currentLongitude = latLng.lng();
-        var latlng = {lat: currentLatitude, lng: currentLongitude};
-        var geocoder = new google.maps.Geocoder;
-        geocoder.geocode({'location': latlng}, function(results, status) {
-              if (status === 'OK') {
-                if (results[0]) {
-                  input.value = results[0].formatted_address;
-                } else {
-                  window.alert('No results found');
-                }
-              } else {
-                window.alert('Geocoder failed due to: ' + status);
-              }
-            });
-    });
-
-    google.maps.event.addListener(autocomplete, "place_changed", function()
-    {
-        var place = autocomplete.getPlace();
-
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(15);
-        }
-
-        marker.setPosition(place.geometry.location);
-    });
+google.maps.event.addListener(marker, 'dragend', function(marker){
+    var latLng = marker.latLng;
+    currentLatitude = latLng.lat();
+    currentLongitude = latLng.lng();
+    var latlng = {lat: currentLatitude, lng: currentLongitude};
+    var geocoder = new google.maps.Geocoder;
+    geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[0]) {
+              input.value = results[0].formatted_address;
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+});
 
 
-$("#myModal").on("shown.bs.modal", function () {
+google.maps.event.addListener(autocomplete, "place_changed", function()
+{
+    var place = autocomplete.getPlace();
+
+    if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+    } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(15);
+    }
+
+    marker.setPosition(place.geometry.location);
+});
+
+var clicked_by = "";
+
+$("#myModal").on("shown.bs.modal", function (e) {
     google.maps.event.trigger(map, "resize");
     map.setCenter(defaultLatLong);
+    clicked_by = e.relatedTarget.id;
+
+});
+
+$('#myModal').on('hidden.bs.modal', function (e) {
+    var text = document.getElementById("location_input").value;
+    if (clicked_by === "source_location_button") {
+        document.getElementById('id_source_location').value = text;
+    }
+    else if(clicked_by === "destination_location_button") {
+        document.getElementById('id_destination_location').value = text;
+    }
 });
 
 //Logout Redirection
